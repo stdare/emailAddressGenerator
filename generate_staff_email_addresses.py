296cd4e22bdfb0966855ac4b44bb38c7735b2ed7
Daniel S <stdare@users.noreply.github.com>
2019-01-21 10:24:05 +1100

    Update generate_staff_email_addresses.py

--- generate_staff_email_addresses.py
+++ generate_staff_email_addresses.py
@@ -1,7 +1,7 @@
 """
 This script will open a .txt file containing staff names,
 remove spaces and replace with '.' then generate the full email address,
-sending it to a .txt file which then opens in notepad.
+sending it to a .txt file which then opens in Notepad.
 """
 
 #read contents of .csv file with just staff names without email addresses.
